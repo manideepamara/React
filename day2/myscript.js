2e599fbb588074719db4aeda781768253d201edb
manideepamara <manideepgupta144@gmail.com>
2020-10-26 00:36:04 +0800

    addeing day2 and practice

---
+++ day2/myscript.js
@@ -0,0 +1,5 @@
+function add(){
+			var f = parseInt(document.getElementById("f").value);
+			var s = parseInt(document.getElementById("s").value);
+			alert("addition of two numbers is"+(f+s));
+		}
